baf075366c8937267f3f59c131e6d524fdf6f957
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-03 17:27:50 +0900

    refactor: split to functions

--- main.ts
+++ main.ts
@@ -1,27 +1,29 @@
 import { Buffer, readAll, serve } from "./deps.ts";
 import { parseWithNeologd } from "./parser.ts";
 
-const port = 8080;
 const PARSE_ROUTE = new URLPattern({ pathname: "/parse" });
 const isStringArray = (value: unknown): value is string[] => {
   return Array.isArray(value) &&
     value.every((val) => typeof val === "string");
 };
 
-serve(async (request: Request) => {
-  if (request.method != "POST" || !PARSE_ROUTE.exec(request.url)) {
-    return new Response(`No route for ${request.url}`, { status: 404 });
-  }
-  if (request.headers.get("Content-Type") != "application/json") {
-    return new Response("Content-Type must be application/json", {
+const convertFromBodyToJson = async (body: ReadableStream<Uint8Array> | null) => {
+  const reader = await body?.getReader().read();
+  const buf = await readAll(new Buffer(reader?.value));
+  const decoder = new TextDecoder();
+
+  return JSON.parse(decoder.decode(buf))
+}
+
+const parse_handler = async (request: Request) => {
+  const expectedContentType = "application/json"
+  if (request.headers.get("Content-Type") != expectedContentType) {
+    return new Response(`Content-Type must be ${expectedContentType}`, {
       status: 400,
     });
   }
 
-  const reader = await request.body?.getReader().read();
-  const buf = await readAll(new Buffer(reader?.value));
-  const decoder = new TextDecoder();
-  const texts = JSON.parse(decoder.decode(buf)).texts;
+  const texts = (await convertFromBodyToJson(request.body)).texts;
   if (!isStringArray(texts)) {
     return new Response("Posted json has illegal type", { status: 400 });
   }
@@ -33,8 +35,16 @@ serve(async (request: Request) => {
   const responseJson = JSON.stringify(parsed.value);
 
   return new Response(responseJson);
+};
+
+serve(async (request: Request) => {
+  if (request.method == "POST" && PARSE_ROUTE.exec(request.url)) {
+    return await parse_handler(request);
+  }
+
+  return new Response(`No route for ${request.url}`, { status: 404 });
 }, {
-  port,
+  port: 8080,
   onListen({ hostname, port }) {
     console.log(`HTTP server listening on http://${hostname}:${port}`);
   },
