0f071b874285c8885fe92d2b414b2f13bf1bb52d
akky <akimoto+github@gmail.com>
2022-11-24 21:54:30 +0900

    added wakati API

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { Buffer, readAll, serve } from "./deps.ts";
-import { parseWithNeologd } from "./parser.ts";
+import { parseWithNeologd, wakatiWithNeologd } from "./parser.ts";
 import { tryCatch } from "./result.ts";
 
 const PARSE_ROUTE = new URLPattern({ pathname: "/parse" });
@@ -37,19 +37,57 @@ const parse_handler = async (request: Request) => {
   }
 
   const responseJson = JSON.stringify(parsed.value);
-
+  //console.log(parsed.value);
   return new Response(responseJson);
 };
 
+//----------------------------
+const WAKATI_ROUTE = new URLPattern({ pathname: "/wakati" });
+
+const wakati_handler = async (request: Request) => {
+  const expectedContentType = "application/json";
+  if (request.headers.get("Content-Type") != expectedContentType) {
+    return new Response(`Content-Type must be ${expectedContentType}`, {
+      status: 400,
+    });
+  }
+
+  const wakatiedTexts = await convertFromBodyToJsonText(request.body);
+  if (wakatiedTexts.isFailure() || !isStringArray(wakatiedTexts.value.texts)) {
+    return new Response("Posted json has illegal type", { status: 400 });
+  }
+  const wakatied = await wakatiWithNeologd(wakatiedTexts.value.texts);
+  if (wakatied.isFailure()) {
+    console.error(`Wakati Error!: ${wakatied.error}`);
+    return new Response("Wakati Error", { status: 500 });
+  }
+  // wakati の時、surface しか意味のある結果が入らないのでそれだけ残す
+  //  配列の2つめに空文字の結果が来るので、これは無視
+  //  keyを 'surface' から 'wakati'へ変更
+  console.log(wakatied.value);
+  const filtered = wakatied.value.map(function(outer) { return { 'wakati': outer[0].surface } });
+  console.log(filtered);
+
+  const responseJson = JSON.stringify(filtered);
+  return new Response(responseJson);
+};
+
+//----------------------------
 serve(async (request: Request) => {
   if (request.method == "POST" && PARSE_ROUTE.exec(request.url)) {
     return await parse_handler(request);
   }
 
+  if (request.method == "POST" && WAKATI_ROUTE.exec(request.url)) {
+    return await wakati_handler(request);
+  }
+
   // GET request handler for server health check
   const PARSE_HEALTH = new URLPattern({ pathname: "/health" })
   if (request.method == "GET" && PARSE_HEALTH.exec(request.url)) {
-    return new Response(`Mecab API server is alive for ${request.url}`, { status: 200 });
+    //return new Response(`Mecab API server is alive for ${request.url}`, { status: 200 });
+    const responseJson = JSON.stringify({ status: 'ok', code: 200 });
+    return new Response(responseJson, { status: 200 });
   }
 
   return new Response(`No route for ${request.url}`, { status: 404 });
--- parser.ts
+++ parser.ts
@@ -27,3 +27,31 @@ export const parseWithNeologd = async (texts: string[]) => {
 
   return success(await parseWith(mecab.value, texts));
 };
+
+// --- wakati
+
+const genMecabWakati = (dicPathEnvName?: string) => {
+  const commands = ["mecab"];
+  commands.push("-Owakati");
+  if (dicPathEnvName) {
+    if (env.has(dicPathEnvName)) commands.push("-d", env.get(dicPathEnvName));
+    else return failure(new Error("The env is not set"));
+  }
+
+  return success(new MeCab(commands));
+};
+
+const wakatiWith = (lib: MeCab, texts: string[]) =>
+  Promise.all(texts.map(async (s) => await lib.parse(s)));
+
+export const wakati = async (texts: string[]) =>
+  await wakatiWith(unwrap(genMecabWakati()), texts);
+
+export const wakatiWithNeologd = async (texts: string[]) => {
+  const mecab = genMecabWakati("NEOLOGD_DIC_PATH");
+  if (mecab.isFailure()) {
+    return failure(mecab.error);
+  }
+
+  return success(await wakatiWith(mecab.value, texts));
+};
